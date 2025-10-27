33066b298a63fd28799687937925e9b48fe1b220
KaeTheDev <kaethedev@gmail.com>
2025-10-27 13:25:47 -0400

    Added function that removes last item from shoppingList array

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // Task 1: Array Manipulation Basics
 const shoppingList = []
 function addItem(item) {
-    shoppingList.push(item);
+    shoppingList.push(item)
+}
+
+function removeLastItem(item) {
+    shoppingList.pop(item)
 }
 
 // Task 2: Filter and Search an Array
