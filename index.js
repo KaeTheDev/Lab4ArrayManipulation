300d6768b16647cf3f6f62bcc1eae832ecf362e1
KaeTheDev <kaethedev@gmail.com>
2025-10-27 13:30:48 -0400

    Added console.log tests to see if the functions are working correctly

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const shoppingList = []
 function addItem(item) {
     shoppingList.push(item)
 }
+console.log(addItem("test"))
+console.log(addItem("second test"))
+
+console.log(shoppingList)
 
 function removeLastItem(item) {
     shoppingList.pop(item)
 }
+console.log(removeLastItem())
+console.log(shoppingList)
 
 // Task 2: Filter and Search an Array
 
